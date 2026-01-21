1feea46531ca90775e3c920e55467dc1e9a220c4
Jackson Bossart <54560777+Savijack@users.noreply.github.com>
2026-01-21 22:19:32 +0000

    copy except test docs

---
+++ HTMLConverter.cpp
@@ -0,0 +1,55 @@
+#include "HTMLConverter.h"
+
+HTMLConverter::HTMLConverter(const std::string &filepath)
+{
+   ifstream file(filepath);
+   string line;
+
+   if (!file.is_open()) {
+       cerr << "Error: Could not open the file." << endl;
+       exit(0);
+   }
+
+   while (getline(file, line)) {
+       lines.push_back(line);
+   }
+
+   file.close();
+   for (int i = 0; i < lines.size(); i++)
+   {
+       cout << lines[i] << endl;
+   }
+}
+
+void HTMLConverter::convert(const string& outputFilepath)
+{
+    ofstream outputFile;
+    // Specify the filename with the .html extension
+    outputFile.open(outputFilepath);
+
+    if(outputFile.is_open())
+    {
+        cout << "file opened" << endl;
+    }
+    else
+    {
+        cout << "file didn't open" << endl;
+    }
+
+    // Write HTML content as strings
+    outputFile << "<!DOCTYPE html>\n";
+    outputFile << "<html>\n";
+    outputFile << "<body>\n";
+
+    for(int i = 0; i < output.size(); i++)
+    {
+        outputFile << output[i];
+    }
+
+    outputFile << "</body>\n";
+    outputFile << "</html>\n";
+    // Close the file
+    outputFile.close();
+
+    std::cout << "HTML file 'output.html' generated successfully." << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 #define CATCH_CONFIG_NO_POSIX_SIGNALS
 #define CATCH_CONFIG_MAIN   
 #include "catch.hpp"
+#include "HTMLConverter.h"
+#include <iostream>
+
 using namespace std;
 
+int main()
+{
+   HTMLConverter *test = new HTMLConverter("./test_documents/1.md");
+   test->convert("output.html");
+   delete test;
+}
+
 //--
-TEST_CASE("Test Template")  
+TEST_CASE("File Conversion")  
 { 
-	REQUIRE(100 == 100); 
+	SECTION("")
+	{
+		REQUIRE(10 == 10);
+	}
+
+	SECTION("")
+	{
+		REQUIRE(10 == 10);
+	}
 }
