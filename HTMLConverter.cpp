6243b91b106b7b9aea3f6fb5069d318ce9d901b0
Ryan Heffelman <132711854+rheffelman@users.noreply.github.com>
2026-01-22 15:57:58 +0000

    added a bunch of methods that use regex to detect markdown syntax

--- HTMLConverter.cpp
+++ HTMLConverter.cpp
@@ -53,3 +53,66 @@ void HTMLConverter::convert(const string& outputFilepath)
 
     std::cout << "HTML file 'output.html' generated successfully." << std::endl;
 }
+bool HTMLConverter::hasBoldAndItalics(const string& line) {
+    static const regex pattern(R"(\*{3}.+?\*{3})");
+    return regex_search(line, pattern);
+}
+bool HTMLConverter::hasBold(const string& line) {
+    // assumes boldAndItalics are already handled
+    static const regex pattern(R"(\*{2}.*\*{2})");
+    return regex_search(line, pattern);
+}
+bool HTMLConverter::hasItalics(const string& line) {
+    // assumes bold is already handled
+    static const regex pattern(R"(\*(?!\*)([^*]+)\*)");
+    return regex_search(line, pattern);
+}
+bool HTMLConverter::hasLine(const string& line) {
+    static const regex pattern(R"(^\s*---\s*$)");
+    return regex_search(line, pattern);
+}
+bool HTMLConverter::hasLink(const string& line) {
+    static const regex pattern(R"((^|[^!])\[[^\]]+\]\([^)]+\))");
+    return regex_search(line, pattern);
+}
+bool HTMLConverter::hasImage(const string& line) {
+    static const regex pattern(R"(!\[[^\]]+\]\([^)]+\))");
+    return regex_search(line, pattern);
+}
+bool HTMLConverter::hasCodeBlock(const string& line) {
+    static const regex pattern(R"(`{3})");
+    return regex_search(line, pattern);
+}
+bool HTMLConverter::hasList(const string& line) {
+    static const regex dash_pattern(R"(^\s{0,3}-\s+)");
+    static const regex number_pattern(R"(^\s{0,3}\d+\.\s+)");
+    return regex_search(line, dash_pattern) or regex_search(line, number_pattern);
+}
+bool HTMLConverter::hasCodeHighlight(const string& line) {
+    static const regex pattern(R"(\[<[^>]+>\])");
+    return regex_search(line, pattern);
+}
+bool HTMLConverter::hasTable(const string& line) {
+    static const regex pattern(R"(^:::)");
+    return regex_search(line, pattern);
+}
+bool HTMLConverter::hasFilename(const string& line) {
+    static const regex pattern(R"(```\s{1}file=\".*\")");
+    return regex_search(line, pattern);
+}
+// RETURNS:
+// 1-6 depending on the number of #'s
+// 0 if there's no #, or it's not in the correct position, or it's not followed by a space.
+int HTMLConverter::hasHeader(const string& line) {
+    int count = 0;
+
+    while (count < line.size() and line[count] == '#') {
+        count++;
+    }
+ 
+    // if it has one or more #'s, followed by a space 
+    if (count > 0 and line[count] == ' ') {
+        return count;
+    }
+    return 0;
+}
--- HTMLConverter.h
+++ HTMLConverter.h
@@ -2,7 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
-
+#include <regex>
 using namespace std;
 
 class HTMLConverter {
@@ -19,5 +19,19 @@ private:
    vector<string> lines;
    vector<string> output;
    string filepath;
+
+   // some of these are specific to C++ regex and might not work if compiled with other regex engines
+   bool hasBoldAndItalics(const string& line); // matches ***text***
+   bool hasBold(const string& line);           // matches **text**
+   bool hasItalics(const string& line);        // matches *text*
+   bool hasLine(const string& line);           // matches ---
+   bool hasLink(const string& line);           // matches [YouTube](youtube.com)
+   bool hasImage(const string& line);          // matches ![alt text](path/to/photo.png)
+   bool hasCodeBlock(const string& line);      // matches ```
+   bool hasList(const string& line);           // matches dashed or numbered lists (strings starting with - or a number + .)
+   bool hasCodeHighlight(const string& line);  // matches [<code>]
+   bool hasTable(const string& line);          // matches :::
+   bool hasFilename(const string& line);       // matches ``` filename=""
+   int hasHeader(const string& line);          // returns number of #'s at the start of line, followed by space
 
 };
